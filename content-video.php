ee33172ff5381484f55b8e8f90da68b916399229
Sami Keijonen <sami.keijonen@foxnet.fi>
2013-06-08 20:06:35 +0300

    Use the_remaining_content and show video above title only if the_remaining_content exists.

--- content-video.php
+++ content-video.php
@@ -2,17 +2,24 @@
 
 	<?php if ( is_singular( get_post_type() ) ) { ?>
 	
-		<div class="entry-media">
-			<?php the_post_format_video(); ?>
-		</div><!-- .entry-media -->
-
+		<?php if ( function_exists( 'the_remaining_content' ) ) { ?>
+			<div class="entry-media">
+				<?php the_post_format_video(); ?>
+			</div><!-- .entry-media -->
+		<?php } ?>
+		
 		<header class="entry-header">
 			<h1 class="entry-title"><?php single_post_title(); ?></h1>
 			<?php echo apply_atomic_shortcode( 'entry_byline', '<div class="entry-byline">' . __( '[post-format-link] published on [entry-published] [entry-comments-link before=" | "] [entry-edit-link before=" | "]', 'eino' ) . '</div>' ); ?>
 		</header><!-- .entry-header -->
 
 		<div class="entry-content">
-			<?php the_remaining_content(); ?>
+			<?php 
+			if ( function_exists( 'the_remaining_content' ) ) 
+				the_remaining_content();
+			else
+				the_content();
+			?>
 			<?php wp_link_pages( array( 'before' => '<p class="page-links">' . '<span class="before">' . __( 'Pages:', 'eino' ) . '</span>', 'after' => '</p>' ) ); ?>
 		</div><!-- .entry-content -->
 
@@ -35,14 +42,7 @@
 			<div class="entry-summary">
 				<?php the_excerpt(); ?>
 			</div><!-- .entry-summary -->
-
-		<?php } else { ?>
-
-			<div class="entry-content">
-				<?php the_remaining_content( __( 'Read more &rarr;', 'eino' ) ); ?>
-				<?php wp_link_pages( array( 'before' => '<p class="page-links">' . '<span class="before">' . __( 'Pages:', 'eino' ) . '</span>', 'after' => '</p>' ) ); ?>
-			</div><!-- .entry-content -->
-
+			
 		<?php } ?>
 
 		<footer class="entry-footer">
