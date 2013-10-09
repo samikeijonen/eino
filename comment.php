d1a26882ee71618607fbcd395952dec40b8e4d80
Sami Keijonen <sami.keijonen@foxnet.fi>
2013-10-09 20:25:11 +0300

    Update comment meta.

--- comment.php
+++ comment.php
@@ -4,7 +4,7 @@
 
 		<?php echo hybrid_avatar(); ?>
 
-		<?php echo apply_atomic_shortcode( 'comment_meta', '<div class="comment-meta"><span class="comment-author vcard">[comment-author]</span> <span class="published">[comment-published]</span> [comment-permalink before="&middot; "] [comment-edit-link before="&middot; "] [comment-reply-link before="&middot; "]</div>' ); ?>
+		<?php echo apply_atomic_shortcode( 'comment_meta', '<div class="comment-meta">[comment-author] [comment-published] [comment-permalink before="&middot; "] [comment-edit-link before="&middot; "] [comment-reply-link before="&middot; "]</div>' ); ?>
 
 		<div class="comment-content">
 			<?php comment_text(); ?>
